35665d151dcc60f409df1c27c54d3d10920dee7b
Finck Parker <parkms940@naver.com>
2018-12-06 06:32:09 +0900

    add some comments

--- push_list.sol
+++ push_list.sol
@@ -5,14 +5,17 @@ contract _people {
     
     uint public index;
     
+    // 구조 생성
     struct Person {
         uint    age;
         string  name;
         uint    index;
     }
     
+    // 구조의 동적배열 생성
     Person[] people;
     
+    // 새 구조 생성해서 배열에 넣기
     function createpeople(uint age, string name) public returns (uint){
         Person memory person = Person(age, name, index);
         people.push(person);
@@ -20,6 +23,7 @@ contract _people {
         index ++;
     }
     
+    // 배열 속 구조 반환
     function getperson (uint i) public returns (string) {
         return people[i].name;
     }
