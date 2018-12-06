b436a3719df90047b4ac739213381f5add5f61b4
Finck Parker <parkms940@naver.com>
2018-12-06 19:08:42 +0900

    second upload

---
+++ storage.sol
@@ -0,0 +1,13 @@
+programa solidity  ^ 0.4.0;
+
+contract SimpleStorage{
+    uint storedData;
+
+    function set (uint x){
+       storedData = x;
+    }
+
+    function get() constant returns (uint retVal){
+        return storedData;
+    }
+}
