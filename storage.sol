a9c898ae7ad7ab70a0119a73ee19acc23cdcb56b
Finck Parker <parkms940@naver.com>
2019-01-02 17:34:29 +0900

    change to version 5

--- storage.sol
+++ storage.sol
@@ -1,4 +1,4 @@
-pragma solidity  ^ 0.4.0;
+pragma solidity  ^ 0.5.0;
 
 contract SimpleStorage{
     uint storedData;
@@ -7,7 +7,7 @@ contract SimpleStorage{
         storedData = x;
     }
 
-    function get() view returns (uint){
+    function get() public view returns (uint){
         return storedData;
     }
 
