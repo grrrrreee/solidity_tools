c63343d1cdc1cf71e58718cf34e2d4e33bfba654
Finck Parker <parkms940@naver.com>
2019-01-04 03:52:48 +0900

    upgrade

--- voting.sol
+++ voting.sol
@@ -10,18 +10,28 @@ contract voting {
         bool        voted; // 이 부분은 나중에 따로 생각해봐야 함
     }
     
-    mapping (address => voter);
+    mapping (address => uint)      _member;
+    mapping (uint => proposal)      _vote;
+    
+    voter[] people;
+    
+    function create_people(uint index, address v_address, string memory name) public {
+        
+        voter memory person = voter(index, v_address, name, false);
+        people.push(person);
+        
+        _member[msg.sender] = index;
+        index ++ ; 
+    }
     
     struct proposal {
         uint        p_index;
-        address     p_address; // 이 부분은 나중에 따로 생각해봐야 함
-        string      p_name;
+        address     p_address;  // 이 부분은 나중에 따로 생각해봐야 함
+        string      p_name;     // 투표 이름
+        string      _name;      // 제안자 이름
         uint        voted;
         uint        agree;
         uint        disagree;
     }
-    
-    function create_proposal(string memory _p_name) public {
-        v_address = msg.sender;
-    }
+
 }
