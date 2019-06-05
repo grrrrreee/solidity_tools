fc845d50a66756ce6c425c8e123a4657e729667d
finchparker <parkms940@naver.com>
2019-06-05 13:32:03 +0900

    수정후 추가

--- hashing.sol
+++ hashing.sol
@@ -1,11 +1,24 @@
 pragma solidity ^ 0.5.0;
 
-contract hashing {
+contract hashing1 {
     uint public index; 
     
     function hash (string memory word) public returns(bytes32) {
         
         bytes32 hash_result = keccak256(abi.encodePacked(index, word));
+        
+        index ++;
+        
+        return hash_result;
+    }
+}
+
+contract hashing2 {
+    uint public index; 
+    
+    function hash (string memory word) public returns(bytes32) {
+        
+        bytes32 hash_result = sha256(abi.encodePacked(index, word));
         
         index ++;
         
